aab954bbb32a5271db8e731a4dd2a4e6369ea6c9
Svanstedt <nils.svanstedt@gmail.com>
2024-01-27 11:24:58 +0100

    Rename backend components to use consistent 'recipe' naming convention

--- server/src/router/recipe.ts
+++ server/src/router/recipe.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { Recipe } from "../model/recipe";
-import { RecipeService } from "../service/recipeService";
+import { RecipeService } from "../service/recipe";
 
 const recipeService = new RecipeService();
 
