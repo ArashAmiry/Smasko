f662f0f8905bae5d49284d85523154988afeb52e
antonboras <boras@net.chalmers.se>
2024-02-13 12:12:30 +0100

    css to match our mockup
    Co-Authored-By: Arash Amiry <ArashAmiry@hotmail.com>
    Co-Authored-By: Nils Bengtsson Svanstedt <48283844+Svanstedt@users.noreply.github.com>
    Co-Authored-By: Gustav Berndtzen <93664614+GurraBern@users.noreply.github.com>

--- client/src/CreateRecipe.tsx
+++ client/src/CreateRecipe.tsx
@@ -7,6 +7,7 @@ import Form from "react-bootstrap/esm/Form";
 import Container from "react-bootstrap/esm/Container";
 import Button from "react-bootstrap/esm/Button";
 import axios from "axios";
+import Col from "react-bootstrap/esm/Col";
 
 
 export type Ingredient = {
@@ -24,50 +25,50 @@ interface IngredientsListProps {
 }
 
 function CreateRecipe() {
-    const [ingredientsList, setIngredientsList] = useState<Ingredient[]>([{name : "", amount: 0, unit: "st"}]);
+    const [ingredientsList, setIngredientsList] = useState<Ingredient[]>([{ name: "", amount: 0, unit: "st" }]);
     const [stepsList, setStepsList] = useState<string[]>([""]);
     const [recipeName, setRecipeName] = useState("");
     const [imgPath, setImgPath] = useState("");
     const [numServings, setNumServings] = useState(4);
 
     const addIngredient = () => {
-        let newIngredient : Ingredient = {name : "", amount: 0, unit: "st"};
+        let newIngredient: Ingredient = { name: "", amount: 0, unit: "st" };
         setIngredientsList([...ingredientsList, newIngredient]);
         //console.log("hej");
     }
 
-    const deleteIngredient = (index : number) => {
+    const deleteIngredient = (index: number) => {
         //console.log(index);
         const list = [...ingredientsList];
         list.splice(index, 1);
         setIngredientsList(list);
         //console.log(list);
     }
-    const changeName = (name : string, index : number) => {
+    const changeName = (name: string, index: number) => {
         const list = [...ingredientsList];
-       // console.log(name);
+        // console.log(name);
         list[index].name = name;
         setIngredientsList(list);
     }
-    const changeAmount = (amount : number, index : number) => {
+    const changeAmount = (amount: number, index: number) => {
         const list = [...ingredientsList];
-       // console.log(name);
+        // console.log(name);
         list[index].amount = amount;
         setIngredientsList(list);
     }
-    const changeUnit = (unit : string, index : number) => {
+    const changeUnit = (unit: string, index: number) => {
         const list = [...ingredientsList];
-       // console.log(name);
+        // console.log(name);
         list[index].unit = unit;
         setIngredientsList(list);
     }
 
     const addStep = () => {
-        setStepsList([...stepsList,  ""]);
+        setStepsList([...stepsList, ""]);
         //console.log("hej");
     }
 
-    const deleteStep = (index : number) => {
+    const deleteStep = (index: number) => {
         //console.log(index);
         const list = [...stepsList];
         list.splice(index, 1);
@@ -75,91 +76,100 @@ function CreateRecipe() {
         //console.log(list);
     }
 
-    const handleChangeStep = (e : ChangeEvent, index : number) => {
-        const {value} = e.target as HTMLInputElement;
+    const handleChangeStep = (e: ChangeEvent, index: number) => {
+        const { value } = e.target as HTMLInputElement;
         const list = [...stepsList];
-       // console.log(name);
+        // console.log(name);
         list[index] = value;
         setStepsList(list);
         //console.log(ingredientsList)
     }
 
-    const changeRecipeName = (e : ChangeEvent) => {
-         const { value } = e.target as HTMLInputElement; 
-         setRecipeName(value);
+    const changeRecipeName = (e: ChangeEvent) => {
+        const { value } = e.target as HTMLInputElement;
+        setRecipeName(value);
     }
 
-    const changeNumServings = (e : ChangeEvent) => {
+    const changeNumServings = (e: ChangeEvent) => {
         const { value } = e.target as HTMLInputElement;
         setNumServings(parseInt(value));
     }
 
-    async function submitRecipe(e : FormEvent)  {
+    async function submitRecipe(e: FormEvent) {
         e.preventDefault();
         const response = await axios.post('http://localhost:8080/recipe', {
-        "name": recipeName,
-        "imagePath": "hej",
-        "numberServings": numServings,
-        "ingredients": ingredientsList,
-        "steps": stepsList
-        }, {timeout: 10000})
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        }); 
+            "name": recipeName,
+            "imagePath": "hej",
+            "numberServings": numServings,
+            "ingredients": ingredientsList,
+            "steps": stepsList
+        }, { timeout: 10000 })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
         window.location.reload();
     }
 
     return (
         <Form className="container m-2 mx-auto create-recipe-container" onSubmit={e => submitRecipe(e)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="text" placeholder="Recipe Name" name="recipeName" onChange={e => changeRecipeName(e)}/>
+                <Form.Control
+                    className="recipe-name mx-auto mt-3"
+                    type="text"
+                    placeholder="Recipe Name"
+                    name="recipeName"
+                    required
+                    maxLength={20}
+                    onChange={e => changeRecipeName(e)} />
             </Form.Group>
-            <Form.Group className="test">
+            <Form.Group className="image-input my-3 p-4">
                 <Form.Label> Choose an image for the recipe</Form.Label>
                 <Form.Control type="file" lang="en" />
             </Form.Group>
 
-            <Form.Group className="ingredients my-3 px-3">
+            <Form.Group as={Col} sm className="ingredients justify-content-center mt-3 rounded-4 bg-light shadow-sm p-4">
                 <h2>Ingredients</h2>
-                <Form.Label>Number of servings</Form.Label>
-                <Form.Select defaultValue={4} onChange={e => changeNumServings(e)}>
-                    <option value="2">2</option>
-                    <option value="4">4</option>
-                    <option value="6">6</option>
-                    <option value="8">8</option>
-                    <option value="10">10</option>
-                </Form.Select>
+                <Container className="servings-container col-md-1 mb-3" >
+                    <Form.Label>Number of servings</Form.Label>
+                    <Form.Select defaultValue={4} onChange={e => changeNumServings(e)}>
+                        <option value="2">2</option>
+                        <option value="4">4</option>
+                        <option value="6">6</option>
+                        <option value="8">8</option>
+                        <option value="10">10</option>
+                    </Form.Select>
+                </Container>
 
                 <Container className="p-0">
-                 <IngredientsList 
-                    ingredientsList={ingredientsList}
-                    deleteIngredient={deleteIngredient}
-                    changeName={changeName}
-                    changeAmount={changeAmount}
-                    changeUnit={changeUnit}
-                 />
-                 </Container>
-                <Button variant="primary" onClick={addIngredient}>
+                    <IngredientsList
+                        ingredientsList={ingredientsList}
+                        deleteIngredient={deleteIngredient}
+                        changeName={changeName}
+                        changeAmount={changeAmount}
+                        changeUnit={changeUnit}
+                    />
+                </Container>
+                <Button variant="outline-secondary" className="btn-outline-secondary mt-3" onClick={addIngredient}>
                     Add new ingredient
                 </Button>
             </Form.Group>
 
-            <Form.Group>
+            <Form.Group as={Col} sm className="ingredients justify-content-center mt-3 rounded-4 bg-light shadow-sm p-4">
                 <Container className="p-0">
-                {stepsList.map((singleStep, index) => (
-                    <StepRow 
-                    key={index} 
-                    step={singleStep} 
-                    handleDelete={() => deleteStep(index)} 
-                    handleChange={(e) => handleChangeStep(e, index)} 
-                    index={index}
-                    />
-                ))}
-            </Container>
-                <Button variant="primary" onClick={addStep}>
+                    {stepsList.map((singleStep, index) => (
+                        <StepRow
+                            key={index}
+                            step={singleStep}
+                            handleDelete={() => deleteStep(index)}
+                            handleChange={(e) => handleChangeStep(e, index)}
+                            index={index}
+                        />
+                    ))}
+                </Container>
+                <Button variant="outline-secondary" className="btn-outline-secondary my-3" onClick={addStep}>
                     Add new step
                 </Button>
             </Form.Group>
@@ -173,7 +183,7 @@ function CreateRecipe() {
     );
 }
 
-function IngredientsList({ingredientsList, deleteIngredient, changeName, changeAmount, changeUnit} : IngredientsListProps) {
+function IngredientsList({ ingredientsList, deleteIngredient, changeName, changeAmount, changeUnit }: IngredientsListProps) {
     return (
         <>
             {ingredientsList.map((singleIngredient, index) => (
--- client/src/RecipeDetails.tsx
+++ client/src/RecipeDetails.tsx
@@ -6,32 +6,35 @@ import Form from "react-bootstrap/esm/Form";
 import Row from "react-bootstrap/esm/Row";
 import Image from 'react-bootstrap/Image';
 import { useNavigate, useParams } from "react-router-dom";
+import './recipeDetails.css'
 
-interface Recipe{
-    id : number;
-    name : string;
-    imagePath : string;
-    numberServings : number;
-    ingredients : {name : string, amount : number, unit: string}[];
-    steps : string[];
+interface Recipe {
+  id: number;
+  name: string;
+  imagePath: string;
+  numberServings: number;
+  ingredients: { name: string, amount: number, unit: string }[];
+  steps: string[];
 }
 
 function RecipeDetails() {
   const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const [nrServings, setNrServings] = useState(0);
   const { id } = useParams();
   const navigate = useNavigate();
-  
+
   async function fetchRecipe() {
     try {
-        const response = await axios.get(`http://localhost:8080/recipe/${id}`);
-        setRecipe(response.data);
+      const response = await axios.get(`http://localhost:8080/recipe/${id}`);
+      setRecipe(response.data);
+      setNrServings(response.data.numberServings)
     } catch (error) {
-        console.error("Error fetching recipe:", error);
+      console.error("Error fetching recipe:", error);
     }
-    }
+  }
   useEffect(() => {
     fetchRecipe();
-  }, [id]); 
+  }, [id]);
 
   if (!recipe) {
     return <div>Loading...</div>;
@@ -39,7 +42,7 @@ function RecipeDetails() {
 
   const handleDeleteRecipe = () => {
     axios.delete(`http://localhost:8080/recipe/${recipe.id}`)
-      .then(response => { 
+      .then(response => {
         console.log('Recipe deleted successfully');
         navigate('/');
       })
@@ -51,40 +54,44 @@ function RecipeDetails() {
   return (
     <Container className="recipe-container mx-auto">
       <Row className="mx-5">
-        <Image src="https://i.kym-cdn.com/photos/images/original/002/488/662/883.jpg" />
         <h1 className="text-center mx-auto">{recipe.name}</h1>
+        <Image src="https://i.kym-cdn.com/photos/images/original/002/488/662/883.jpg"
+          className="img rounded-4 mt-3 mx-auto px-0" />
+
         <Col sm className="justify-content-center mt-3">
           <div className="rounded-4 bg-light shadow-sm p-4">
             <h2 className="mb-4">Ingredients</h2>
-            <Form.Group as={Col} md="1" className="mb-3">
+            <Form.Group as={Col} md={2} className="mb-3">
               <Form.Label>Servings</Form.Label>
-              <Form.Control as="select" defaultValue={recipe.numberServings}>
-                  <option key={2}>2</option>
-                  <option key={4}>4</option>
-                  <option key={6}>6</option> 
-                  <option key={8}>8</option>
+              <Form.Control as="select" defaultValue={recipe.numberServings} onChange={(e) => setNrServings(Number(e.target.value))}>
+                <option key={2}>2</option>
+                <option key={4}>4</option>
+                <option key={6}>6</option>
+                <option key={8}>8</option>
               </Form.Control>
             </Form.Group>
             <ul>
               {recipe.ingredients.map((ingredient, index) => (
                 <li key={index} className="text-start">
-                   {ingredient.amount} {ingredient.unit} {ingredient.name}
+                  {ingredient.amount * nrServings / recipe.numberServings} {ingredient.unit} {ingredient.name}
                 </li>
               ))}
             </ul>
           </div>
         </Col>
-        <Col sm className="justify-content-center mt-3">  
+        <Col sm className="justify-content-center mt-3">
           <div className="rounded-4 bg-light shadow-sm p-4">
             <h2 className="mb-4">Instructions</h2>
             {recipe.steps.map((step, index) => (
-              <Form.Check 
-                key={index} 
-                type="checkbox" 
-                label={<p><strong>Step {index + 1}:</strong> {step}</p>} 
-                className="text-start"
+              <Form.Check
+                key={index}
+                id={`step-${index}`} // Add a unique id for each checkbox
+                type="checkbox"
+                label={<p><strong>Step {index + 1}:</strong> {step}</p>}
+                className="text-start custom-checkbox"
               />
             ))}
+
           </div>
         </Col>
       </Row>
