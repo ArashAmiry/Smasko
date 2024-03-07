9dd21cf77580e63a1a4d1d6f12857ac12ff64335
antonboras <boras@net.chalmers.se>
2024-03-07 14:29:02 +0100

    moved the heart

--- client/src/components/Recipe/RecipeCard.tsx
+++ client/src/components/Recipe/RecipeCard.tsx
@@ -7,7 +7,7 @@ import Heart from '@react-sandbox/heart'
 import { Row, Col, Container } from 'react-bootstrap';
 import axios from "axios";
 
-function RecipeCard(props: { name: string, img: string, rating: number, like: boolean, id: string, showIngredients: (id : string) => void}) {
+function RecipeCard(props: { name: string, img: string, rating: number, like: boolean, id: string, showIngredients: (id: string) => void }) {
   const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
     props.showIngredients(props.id);
@@ -21,35 +21,32 @@ function RecipeCard(props: { name: string, img: string, rating: number, like: bo
     const clicked = !isClicked;
     setisClick(clicked);
 
-    await axios.patch(`http://localhost:8080/recipe/${props.id}`, 
-      {"liked": clicked}
+    await axios.patch(`http://localhost:8080/recipe/${props.id}`,
+      { "liked": clicked }
     );
   }
 
   return (
-    <Card id={props.id} className='card'>   
-      <Card.Img className="recipeCardImage" variant="top" src={props.img} />
-      <Card.Body className='card-body'>
-        <Container>
-        <Row style={{justifyContent: 'center'}}>
-          <Col sm={10} className='py-1'>
-            <Card.Title className="two-line-name">{props.name}</Card.Title>
-          </Col>
-          <Col sm={2}>
-            <button className='py-0 heartBtn' onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleHeartClick(e)}>
-              <Heart
-                width={24}
-                height={24}
-                active={isClicked}
-                onClick={() => (setisClick(isClicked))}/>
-            </button>
-          </Col>
-        </Row>
-        </Container>
+    <Card id={props.id} className='card'>
+      <Container className="px-0 position-relative" >
+        <Card.Img className="recipeCardImage" variant="top" src={props.img} />
+        <button className='py-0 heartBtn' style={{ position: 'absolute', top: 0, left: 0 }} onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleHeartClick(e)}>
+          <Heart
+            width={40}
+            height={40}
+            active={isClicked}
+            onClick={() => (setisClick(isClicked))}
+            strokeWidth={50}
+          />
+        </button>
+      </Container>
+      <Card.Body className='card-body justify-content-center'>
+              <Card.Title className="two-line-name">{props.name}</Card.Title>
         <Button variant="primary" onClick={(e) => handleClick(e)}>See ingredients</Button>
-        <Rating className="px-auto" initialValue={props.rating} readonly={true} size={30}/>
+        <Rating className="px-auto" initialValue={props.rating} readonly={true} size={30} />
       </Card.Body>
     </Card>
+
   );
 }
 
