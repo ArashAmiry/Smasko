a1dd0762df405cf509854e5f14313788f51c0935
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2024-03-08 15:16:58 +0100

    refactored props

--- client/src/components/Recipe/IngredientView.tsx
+++ client/src/components/Recipe/IngredientView.tsx
@@ -2,13 +2,13 @@ import { Container } from "react-bootstrap";
 import './ingredientView.css';
 import { Ingredient } from "./Ingredient";
 
-function IngredientsView(props: { ingredients: Ingredient[], nrServ: number }) {
+function IngredientsView({ingredients, nrServ} : { ingredients: Ingredient[], nrServ: number }) {
   return (
     <Container className="ingredient-view-container">
       <h1 className="ingredients-title px-5">Ingredients</h1>
-      <h5>Number of servings: {props.nrServ}</h5>
+      <h5>Number of servings: {nrServ}</h5>
       <ul className="ingredients-list">
-        {props.ingredients.map((ingredient, index) => (
+        {ingredients.map((ingredient, index) => (
           <li key={index} className="text-start">
             {
               ingredient.amount
--- client/src/components/Recipe/RecipeCard.tsx
+++ client/src/components/Recipe/RecipeCard.tsx
@@ -4,18 +4,27 @@ import './recipeCard.css';
 import { Rating } from 'react-simple-star-rating';
 import { useState } from 'react';
 import Heart from '@react-sandbox/heart'
-import { Row, Col, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import axios from "axios";
 
-function RecipeCard(props: { name: string, img: string, rating: number, like: boolean, id: string, showIngredients: (id : string) => void}) {
+interface RecipeCardProps {
+  name: string,
+  img: string,
+  rating: number, 
+  like: boolean, 
+  id: string, 
+  showIngredients: (id : string) => void
+}
+
+function RecipeCard({name, img, rating, like, id, showIngredients}: RecipeCardProps) {
 
   // Displays sidebar with the recipes ingredients when clicked
   const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
-    props.showIngredients(props.id);
+    showIngredients(id);
   };
 
-  const [isClicked, setisClick] = useState(props.like);
+  const [isClicked, setisClick] = useState(like);
 
   // Adds recipe to one's favorites
   const handleHeartClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -24,15 +33,15 @@ function RecipeCard(props: { name: string, img: string, rating: number, like: bo
     const clicked = !isClicked;
     setisClick(clicked);
     
-    await axios.patch(`http://localhost:8080/recipe/${props.id}`,
+    await axios.patch(`http://localhost:8080/recipe/${id}`,
       {"liked": clicked}
     );
   }
 
   return (
-    <Card id={props.id} className='card'>
+    <Card id={id} className='card'>
       <Container className="px-0 position-relative" >
-        <Card.Img className="recipeCardImage" variant="top" src={props.img} />
+        <Card.Img className="recipeCardImage" variant="top" src={img} />
         <button className='py-0 heartBtn' style={{ position: 'absolute', top: 0, left: 0 }} onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleHeartClick(e)}>
           <Heart
             width={40}
@@ -44,9 +53,9 @@ function RecipeCard(props: { name: string, img: string, rating: number, like: bo
         </button>
       </Container>
       <Card.Body className='card-body justify-content-center'>
-              <Card.Title className="two-line-name">{props.name}</Card.Title>
+              <Card.Title className="two-line-name">{name}</Card.Title>
         <Button variant="primary" onClick={(e) => handleClick(e)}>See ingredients</Button>
-        <Rating className="px-auto" initialValue={props.rating} readonly={true} size={30} />
+        <Rating className="px-auto" initialValue={rating} readonly={true} size={30} />
       </Card.Body>
     </Card>
   );
